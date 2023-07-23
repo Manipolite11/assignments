c1ea34b53721cd69a1a110050cbc479570195d7d
Manipolite11 <140292271+Manipolite11@users.noreply.github.com>
2023-07-23 08:09:44 +0530

    Add files via upload

---
+++ day1/DataType.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Age = 26;
+
+		float height = 5.4f;
+
+		double salary = 4.32;
+
+		char initial = 'M';
+
+		boolean Car = false;
+
+		String name = "Manikandan";
+
+		System.out.println("Self Intro");
+
+		System.out.println(initial + " " +name);
+
+		System.out.println(height + "Feet");
+
+		System.out.println(Age + " Years old ");
+
+		System.out.println("Having car ?" + Car);
+
+		System.out.println("current CTC " + salary);
+
+	}
+
+}
